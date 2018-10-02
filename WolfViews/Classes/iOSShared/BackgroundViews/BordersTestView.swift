af6596dc6e0f9f29b96eb27897433cf77f99cdaa
Wolf McNally <wolf@wolfmcnally.com>
2018-10-02 11:09:48 -0700

    Moved debugColor() from WolfColor. Commented out BordersTestView.

--- WolfViews/Classes/iOSShared/BackgroundViews/BordersTestView.swift
+++ WolfViews/Classes/iOSShared/BackgroundViews/BordersTestView.swift
@@ -22,54 +22,54 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import UIKit
-import WolfColor
-import WolfAutolayout
-
-public class BordersTestView: View {
-    public override func setup() {
-        contentMode = .redraw
-        backgroundColor = debugColor(.gray, when: true)
-        constrainSize(to: CGSize(width: 300, height: 300))
-    }
-
-    public override func draw(_ rect: CGRect) {
-        let lineWidth: CGFloat = 10
-        let strokeColor = UIColor.red.withAlphaComponent(0.5)
-        let fillColor = UIColor.blue.withAlphaComponent(0.5)
-        var bounds = self.bounds
-
-        func paint(border: Border, in rect: CGRect) {
-            border.draw(in: rect)
-
-            let rectPath = UIBezierPath(rect: rect)
-            rectPath.lineWidth = 0.5
-            UIColor.green.setStroke()
-            rectPath.stroke()
-
-            let insetRect = rect.inset(by: border.makeInsets(in: rect))
-            let path = UIBezierPath(rect: insetRect)
-            path.lineWidth = 0.5
-            UIColor.white.setStroke()
-            path.stroke()
-        }
-
-        do {
-            let border = RectBorder(lineWidth: lineWidth, fillColor: fillColor, strokeColor: strokeColor)
-            paint(border: border, in: bounds)
-        }
-
-        bounds = bounds.insetBy(dx: 30, dy: 30)
-        do {
-            let border = RoundedCornersBorder(lineWidth: lineWidth, cornerRadius: 30, fillColor: fillColor, strokeColor: strokeColor)
-            paint(border: border, in: bounds)
-        }
-
-        bounds = bounds.insetBy(dx: 30, dy: 30)
-        do {
-            let ornaments = CornerOrnaments(topLeft: .square, bottomLeft: .bubbleTail(20), bottomRight: .bubbleTail(20), topRight: .rounded(30))
-            let border = OrnamentedCornersBorder(lineWidth: lineWidth, ornaments: ornaments, fillColor: fillColor, strokeColor: strokeColor)
-            paint(border: border, in: bounds)
-        }
-    }
-}
+//import UIKit
+//import WolfColor
+//import WolfAutolayout
+//
+//public class BordersTestView: View {
+//    public override func setup() {
+//        contentMode = .redraw
+//        backgroundColor = debugColor(.gray, when: true)
+//        constrainSize(to: CGSize(width: 300, height: 300))
+//    }
+//
+//    public override func draw(_ rect: CGRect) {
+//        let lineWidth: CGFloat = 10
+//        let strokeColor = UIColor.red.withAlphaComponent(0.5)
+//        let fillColor = UIColor.blue.withAlphaComponent(0.5)
+//        var bounds = self.bounds
+//
+//        func paint(border: Border, in rect: CGRect) {
+//            border.draw(in: rect)
+//
+//            let rectPath = UIBezierPath(rect: rect)
+//            rectPath.lineWidth = 0.5
+//            UIColor.green.setStroke()
+//            rectPath.stroke()
+//
+//            let insetRect = rect.inset(by: border.makeInsets(in: rect))
+//            let path = UIBezierPath(rect: insetRect)
+//            path.lineWidth = 0.5
+//            UIColor.white.setStroke()
+//            path.stroke()
+//        }
+//
+//        do {
+//            let border = RectBorder(lineWidth: lineWidth, fillColor: fillColor, strokeColor: strokeColor)
+//            paint(border: border, in: bounds)
+//        }
+//
+//        bounds = bounds.insetBy(dx: 30, dy: 30)
+//        do {
+//            let border = RoundedCornersBorder(lineWidth: lineWidth, cornerRadius: 30, fillColor: fillColor, strokeColor: strokeColor)
+//            paint(border: border, in: bounds)
+//        }
+//
+//        bounds = bounds.insetBy(dx: 30, dy: 30)
+//        do {
+//            let ornaments = CornerOrnaments(topLeft: .square, bottomLeft: .bubbleTail(20), bottomRight: .bubbleTail(20), topRight: .rounded(30))
+//            let border = OrnamentedCornersBorder(lineWidth: lineWidth, ornaments: ornaments, fillColor: fillColor, strokeColor: strokeColor)
+//            paint(border: border, in: bounds)
+//        }
+//    }
+//}

fb251f2158e418c833054e2971feb6f280629b27
Wolf McNally <wolf@wolfmcnally.com>
2019-05-28 20:35:48 -0700

    Added PickerView.

---
+++ WolfViews/Classes/iOS/DirectSubclasses/PickerView.swift
@@ -0,0 +1,63 @@
+//
+//  PickerView.swift
+//  WolfViews
+//
+//  Created by Wolf McNally on 5/28/19.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import UIKit
+
+open class PickerView: UIPickerView {
+    public var customBackgroundColor: UIColor?
+    public var customDividerColor: UIColor?
+
+    public convenience init() {
+        self.init(frame: .zero)
+    }
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        _setup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        _setup()
+    }
+
+    private func _setup() {
+        __setup()
+        setup()
+    }
+
+    open func setup() { }
+
+    /// Call from the delegate's pickerView(:viewForRow:forComponent:reusing:) method.
+    public func syncCustomColors() {
+        if let customBackgroundColor = customBackgroundColor {
+            backgroundColor = customBackgroundColor
+        }
+
+        if let customDividerColor = customDividerColor, subviews.count >= 3 {
+            subviews[1].backgroundColor = customDividerColor
+            subviews[2].backgroundColor = customDividerColor
+        }
+    }
+}
